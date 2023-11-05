f6aa7e4861fcf00a10d5ebdd85e00990d00a6596
jorgeUnas <jorgeunas@gmail.com>
2023-11-05 19:22:17 +0000

    Forth exercise done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, ...elems) {
+    for (elem of elems){
+        for (let i = 0; i < myArray.length; i++){
+            if (myArray[i] === elem){
+                myArray.splice(i, 1);
+            }else{
+                continue;
+            }
+        }   
+    }
+    return myArray;
 };
 
 // Do not edit below this line

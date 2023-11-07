345f4540f63d3631f477c08e95390b8eef26268f
jorgeUnas <jorgeunas@gmail.com>
2023-11-07 20:39:18 +0000

    Seventh exercise done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFar) {
+  let tempCel = (tempFar - 32) * 5/9;
+  return +tempCel.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+  let tempFar = (tempCel * 9/5) + 32;
+  return +tempFar.toFixed(1);
 };
 
 // Do not edit below this line
